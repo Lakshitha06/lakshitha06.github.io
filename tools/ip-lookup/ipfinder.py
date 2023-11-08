9a99bd659dc1de3f7f9bdc839e3a0e8d21e1a0ce
Vihanga Lakshitha <148245631+Lakshitha06@users.noreply.github.com>
2023-11-08 10:41:11 +0530

    IP Lookup

---
+++ tools/ip-lookup/ipfinder.py
@@ -0,0 +1,22 @@
+import ipapi
+from flask import Flask, request, render_template
+ 
+#pip install ipapi
+ 
+ 
+app = Flask(__name__)
+ 
+ 
+ 
+@app.route('/', methods = ['GET', 'POST'])
+def Index():
+    search = request.form.get('search')
+    data = ipapi.location(ip=search, output='json')
+    return render_template('index.html', data=data)
+ 
+ 
+ 
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
